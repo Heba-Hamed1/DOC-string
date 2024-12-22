fb2c5812d25cd3746945e9a8e88367f41eb973a0
Heba-Hamed1 <hebahamed681@gmail.com>
2024-12-22 08:40:35 +0200

    Add files via upload

---
+++ doc-string.py
@@ -0,0 +1,18 @@
+def Add_Numbers(x, y):
+    """
+    this function add two number
+    and retuen result 
+      
+    Parametar:
+        x (int):the first number
+        
+        y (int): the second number
+        
+    Return:
+         result(int):the sum of the two numbers
+    
+    """
+    result = x + y
+    return result
+    
+print(Add_Numbers.__doc__)
